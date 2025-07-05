0e58dd4c40ec719a3100f55c1e299a8cac0f2f9d
ankit <ankit.rawat99work@gmail.com>
2025-07-05 16:32:31 +0530

    Updated files with latest changes

--- middleware.js
+++ middleware.js
@@ -24,11 +24,15 @@ module.exports.isOwner =async(req,res,next)=>{
   let { id } = req.params;
     
     let listing = await Listing.findById(id);
-if (!listing.owner.equals(req.user._id)) {
-  req.flash("error", "You are not the owner of this listing");
-  return res.redirect(`/listings/${id}`);
-}
-next();
+    if (!listing) {
+      req.flash("error", "Listing not found!");
+      return res.redirect("/listings");
+    }
+    if (!listing.owner.equals(req.user._id)) {
+      req.flash("error", "You are not the owner of this listing");
+      return res.redirect(`/listings/${id}`);
+    }
+    next();
 };
 
 //validation for schema (middleware)
@@ -55,10 +59,14 @@ module.exports.isReviewAuthor  =async(req,res,next)=>{
   let { id,reviewId } = req.params;
     
     let review = await Review.findById(reviewId);
-if (!review.author.equals(req.user._id)) {
-  req.flash("error", "You did not create this review");
-  return res.redirect(`/listings/${id}`);
-}
-next();
+    if (!review) {
+      req.flash("error", "Review not found!");
+      return res.redirect(`/listings/${id}`);
+    }
+    if (!review.author.equals(req.user._id)) {
+      req.flash("error", "You did not create this review");
+      return res.redirect(`/listings/${id}`);
+    }
+    next();
 };
 
